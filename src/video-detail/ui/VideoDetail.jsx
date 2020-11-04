3445e719b0b987582cd3c540a266c109d22c371c
liangquan <78970494@qq.com>
2020-11-04 20:26:05 +0800

    视频详情页初步

--- src/video-detail/ui/VideoDetail.jsx
+++ src/video-detail/ui/VideoDetail.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import {
     Player,
     BigPlayButton,
   } from 'video-react';
 
 import NavBarRecommend from '@c/recommend/NavBarRecommend'
-import {VideoDetailWrap, WhiteSpace} from './StyledVideoDetail'
+import {VideoDetailWrap, WhiteSpace, VideoFloting} from './StyledVideoDetail'
 
 export default (props) => {
+    const history = useHistory()
+    const { value } = history.location.state
+    console.log(value)
+
     return (
         <VideoDetailWrap>
-          <NavBarRecommend></NavBarRecommend>
+          <NavBarRecommend>{value.title}</NavBarRecommend>
           <WhiteSpace></WhiteSpace>
           <Player
-          poster="https://pic.qyer.com/video/cover/20200916/1600250179253?imageView2/1/w/230/h/130"
-          src='https://media.qyer.com/video/source/20200916/1600250267397'
+          className="player"
+          poster={value.img}
+          src={value.video_url}
           // autoPlay
           >
-            
               <BigPlayButton position="center" />
-            
           </Player>
+          <div className='contain'>
+            <div className="content blur" style={{background:`url(${value.img}) center no-repeat`,backgroundSize:'100% 200%'}}>
+              <div className="mask"></div>
+            </div>
+            <VideoFloting 
+            width="0 0 1px 0"
+            className='floating'>
+              <h3>{value.title}</h3>
+              <p>{value.content}</p>
+              <div className="author">
+                <div className="avatar">
+                  <img src={value.user.logo} alt=""/>
+                </div>
+                <div className="desc">
+                  <h4>{value.user.name}</h4>
+                  <span>{value.user.sign}</span>
+                </div>
+              </div>
+            </VideoFloting>
+          </div>
         </VideoDetailWrap>
     )
 }
